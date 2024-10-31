ec86ab46fd94640b1b8d7f6f00f73b9cf806035a
Nick Babikov <livingstone.doc@yandex.ru>
2024-10-31 20:20:11 +0400

    add: slider

--- src/components/partners-section/Partners.js
+++ src/components/partners-section/Partners.js
@@ -1,3 +1,4 @@
+import Slider from 'react-slick';
 import partnersCaseIcon from '../../assets/icons/partners-section/case.svg';
 import partnerBwImg1 from '../../assets/img/partners-section/alva-bw.png';
 import partnerBwImg2 from '../../assets/img/partners-section/fagor-bw.png';
@@ -11,6 +12,43 @@ import partnerColoredImg4 from '../../assets/img/partners-section/radioshack-col
 import partnerColoredImg5 from '../../assets/img/partners-section/tendo-colored.png';
 import './partners.scss';
 
+function PartnersSlider() {
+  let settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    adaptiveHeight: true
+  };
+  return (
+    <Slider {...settings} className='partners_slider'>
+        <div className='partners_slider__slide'>
+          {/* <img className='bwImg' src={partnerBwImg1} alt='partner-bw1-icon'/> */}
+          <img className='coloredImg' src={partnerColoredImg1} alt='partner-colored1-icon'/>
+        </div>
+        <div className='partners_slider__slide'>
+          {/* <img className='bwImg' src={partnerBwImg5} alt='partner-bw2-icon'/> */}
+          <img className='coloredImg' src={partnerColoredImg5} alt='partner-colored2-icon'/>
+        </div>
+        <div className='partners_slider__slide'>
+          {/* <img className='bwImg' src={partnerBwImg2} alt='partner-bw3-icon'/> */}
+          <img className='coloredImg' src={partnerColoredImg2} alt='partner-colored3-icon'/>
+        </div>
+        <div className='partners_slider__slide'>
+          {/* <img className='bwImg' src={partnerBwImg4} alt='partner-bw4-icon'/> */}
+          <img className='coloredImg' src={partnerColoredImg4} alt='partner-colored4-icon'/>
+        </div>
+        <div className='partners_slider__slide'>
+          {/* <img className='bwImg' src={partnerBwImg3} alt='partner-bw5-icon'/> */}
+          <img className='coloredImg' src={partnerColoredImg3} alt='partner-colored5-icon'/>
+        </div>
+    </Slider>
+  )
+}
+
 function Partners() {
     return (
     <section className='partners'>
@@ -18,7 +56,8 @@ function Partners() {
       <h2 className='partners_title subtitle'>Our Partners</h2>
       <button className='partners_subtitleAndIconWrapper__btn'><img src={partnersCaseIcon} alt='case-icon'/><span>Let's collaborate</span></button>
       </div>
-      <div className='partners_slider'>
+      <PartnersSlider />
+      {/* <div className='partners_slider'>
         <div className='partners_slider__imgWrapper'>
           <img className='bwImg' src={partnerBwImg1} alt='partner-bw1-icon'/>
           <img className='coloredImg' src={partnerColoredImg1} alt='partner-colored1-icon'/>
@@ -39,7 +78,7 @@ function Partners() {
           <img className='bwImg' src={partnerBwImg3} alt='partner-bw5-icon'/>
           <img className='coloredImg' src={partnerColoredImg3} alt='partner-color5-icon'/>
         </div>
-      </div>
+      </div> */}
     </section>
     )
 }
--- src/components/payment-section/Payment.js
+++ src/components/payment-section/Payment.js
@@ -1,28 +1,58 @@
+import Slider from 'react-slick';
 import paymentImg1 from '../../assets/img/payment-section/amex.png';
 import paymentImg2 from '../../assets/img/payment-section/g-pay.png';
 import paymentImg3 from '../../assets/img/payment-section/mastercard.png';
 import paymentImg4 from '../../assets/img/payment-section/sofort.png';
 import paymentImg5 from '../../assets/img/payment-section/union-pay.png';
 import paymentImg6 from '../../assets/img/payment-section/visa.png';
-import arrowLeftIcon from '../../assets/icons/payment-section/left.svg';
-import arrowRightIcon from '../../assets/icons/payment-section/right.svg';
+import prevArrowIcon from '../../assets/icons/payment-section/prevArrow.svg';
+import nextArrowIcon from '../../assets/icons/payment-section/nextArrow.svg';
 import boxIcon from '../../assets/icons/divider/divider-box.svg';
 import './payment.scss';
 
+function PrevArrow(props) {
+    let { onClick } = props;
+    return (
+        <div className='arrowBtn' onClick={onClick}><img src={prevArrowIcon} alt='prev-arrow-icon'/></div>
+    )
+}
+
+function NextArrow(props) {
+    let { onClick } = props;
+    return (
+        <div className='arrowBtn' onClick={onClick}><img src={nextArrowIcon} alt='next-arrow-icon'/></div>
+    )
+}
+
+function PaymentSlider() {
+    let settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: <PrevArrow />,
+        nextArrow: <NextArrow />
+    };
+    return (
+        <Slider {...settings} className='payment_slider'>
+            <div className='payment_slider__slide'><img src={paymentImg2} alt='pay-method1'/></div>
+            <div className='payment_slider__slide'><img src={paymentImg4} alt='pay-method1'/></div>
+            <div className='payment_slider__slide'><img src={paymentImg5} alt='pay-method1'/></div>
+            <div className='payment_slider__slide'><img src={paymentImg6} alt='pay-method1'/></div>
+            <div className='payment_slider__slide'><img src={paymentImg3} alt='pay-method1'/></div>
+            <div className='payment_slider__slide'><img src={paymentImg1} alt='pay-method1'/></div>
+        </Slider>
+    )
+}
+
 function Payment() {
     return (
-        <section className="payment">
-            <h2 className="subtitle">Payment methods</h2>
-            <div className="payment_slider">
-                <button className='arrowBtn arrowLeft'><img src={arrowLeftIcon} alt='arrow-left-icon'/></button>
-                <img src={paymentImg2} alt='pay-method1'/>
-                <img src={paymentImg4} alt='pay-method1'/>
-                <img src={paymentImg5} alt='pay-method1'/>
-                <img src={paymentImg6} alt='pay-method1'/>
-                <img src={paymentImg3} alt='pay-method1'/>
-                <img src={paymentImg1} alt='pay-method1'/>
-                <button className='arrowBtn arrowRight'><img src={arrowRightIcon} alt='arrow-right-icon'/></button>
-            </div>
+        <section className='payment'>
+            <h2 className='subtitle'>Payment methods</h2>
+            <PaymentSlider />
             <img className='dividerIcon' src={boxIcon} alt='box-icon'/>
         </section>
     )
